ec29d20bf624feda721eb527fd1465839e827941
Sören Christ <soeren.christ@bcmsolutions.de>
2022-07-05 14:05:11 +0200

    Add landing page, make auth working with auth0

---
+++ src/components/footer.tsx
@@ -0,0 +1,16 @@
+
+const Footer = () => {
+  return <footer className="flex w-full h-14 bg-gray-700 items-center text-lg text-white flex-row justify-center">
+    <div>
+      <span className="text-gray-400">Built with </span>
+      <a href="https://planetscale.com">Planetscale</a>
+
+      <span> - </span>
+
+      <span className="text-gray-400">Published on </span>
+      <a href="https://hashnode.com">Hashnode</a>
+    </div>
+  </footer>
+}
+
+export default Footer;
