869d7cb621d1a9962b759d4482b968716fec53dc
Sören Christ <soeren.christ@bcmsolutions.de>
2022-07-06 09:11:41 +0200

    Fix button disabled styles

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -2,20 +2,20 @@ import { MouseEventHandler, ReactNode } from "react"
 
 type Props = {
     children?: ReactNode
-    disabled: boolean
+    disabled?: boolean
     type?: "button" | "submit" | "reset"
     onClick?: MouseEventHandler<HTMLButtonElement>
-} & typeof defaultProps;
-
-const defaultProps = {
-    disabled: false
 }
 
 const Button = ({ children, type, disabled, onClick }: Props) => {
+    const disabledStyles = "text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center";
+    const defaultStyles = "mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800";
+
     return (
 
+
         <button
-            className="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            className={`${disabled ? disabledStyles : defaultStyles}`}
             type={type}
             onClick={onClick}
             disabled={disabled}>
@@ -24,5 +24,4 @@ const Button = ({ children, type, disabled, onClick }: Props) => {
     )
 }
 
-Button.defaultProps = defaultProps;
 export default Button;
